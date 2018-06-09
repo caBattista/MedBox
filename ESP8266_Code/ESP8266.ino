66a9fdc5a16b6c61496558643934431d27fdfe70
caBattista <christopherbattista95@gmail.com>
2018-06-09 22:47:59 +0200

    espcode update

--- ESP8266_Code/ESP8266.ino
+++ ESP8266_Code/ESP8266.ino
@@ -1,47 +1,121 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+ESP8266WebServer server(80);
+
+const char* ssid = "TP-LINK_8B1E";
+const char* password = "94077278";
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+Adafruit_SSD1306 display(4); //OLED_RST
+
 #include <Servo.h>
-
 Servo s1;
+const int servoPin = 4;
+const int servoClosed = 50;
+const int servoOpen = 130;
 
-void setup() {
-  s1.attach(14);
-  s1.write(50);
-  delay(300);
-  s1.detach();
-  Serial.begin(115200);
-  Serial.println("Ready");
-}
+#include <NeoPixelBus.h>
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(1, 3);
 
-void loop() {
+int buttonPin = 13;
+
+void setup(void){
+
+  Serial.begin(74880);
+  delay(500);
+
+  strip.Begin();
+  strip.SetPixelColor(0, RgbColor(64,0,64));
+  strip.Show();
   
-  if ( Serial.available()) {
-    char ch = Serial.read();
+  door(servoClosed);
+  
+  pinMode(buttonPin, INPUT_PULLUP);
 
-    switch(ch) {
-      case '1':
-        open(1);
-        break;
-      case '2':
-        open(2);
-        break;
-      case '5':
-        open(5);
-        break;
-    }
+  Wire.begin(14,12);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3c);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Connecting");
+  display.display();
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    //Serial.print(".");
+    display.print(".");
+    display.display();
   }
 
+  display.println("");
+  display.println("Connected to:");
+  display.println(ssid);
+  display.println("IP:");
+  display.println(WiFi.localIP());
+  display.display();
+
+  server.on("/", [](){
+    server.send(200, "text/plain", "hello from esp8266!");
+    displ("/");
+  });
+
+  server.on("/inline", [](){
+    server.send(200, "text/plain", "this works as well");
+    displ("inline");
+  });
+
+  server.begin();
+  display.println("HTTP server started");
+  display.display();
+  
+  while(digitalRead(buttonPin) == 1){
+    delay(100);
+  }
+
+  display.clearDisplay();
+  display.display();
+  strip.SetPixelColor(0, RgbColor(0,0,0));
+  strip.Show();
+  delay(1000);
 }
 
-void open(int j){
-  s1.attach(14);
-  for(int i = 0; i < j; i++){
-    s1.write(130);
-    delay(300);
-    s1.write(50);
-    delay(300);
-    if(j > 1 && i < j){
-      delay(1000);
-      }
+void loop(void){
+  server.handleClient();
+  if(digitalRead(buttonPin) == 0){
     
+    displ("dispensing");
+    strip.SetPixelColor(0, RgbColor(0,64,0));
+    strip.Show();
+    
+    door(servoOpen);
+    door(servoClosed);
+    
+    displ("");
+    strip.SetPixelColor(0, RgbColor(0,0,0));
+    strip.Show();
   }
+}
+
+void displ(String str){
+  display.clearDisplay();   // clears the screen and buffer  
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println(str);
+  display.display();
+}
+
+void door(uint8_t pos){
+  s1.attach(servoPin);
+  s1.write(pos);
+  delay(300);
   s1.detach();
 }
