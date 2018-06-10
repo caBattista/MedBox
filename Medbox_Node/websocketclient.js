0d46dc2d035c388d978f388dba3c60db4e25eeec
caBattista <christopherbattista95@gmail.com>
2018-06-10 14:00:40 +0200

    architektur

--- Medbox_Node/websocketclient.js
+++ Medbox_Node/websocketclient.js
@@ -2,8 +2,52 @@ const WebSocket = require('ws');
 const ws = new WebSocket('ws://localhost:8085');
 const stdin = process.openStdin();
 
+const dispense = {
+  act:"dispense"
+  };
+const setLedAnim = {
+  act:"led",
+  led1:[0,0,64],
+  led2:[0,0,0],
+  time:1000,
+  };
+const display = {
+  act:"display",
+  content: "this is a text",
+  size: 1,
+  clear: 1
+}
+
+function buildJson(input){
+  input = input.split(" ");
+  if(input.length == 8){
+    var action = {
+      act: input[0], 
+      led1: [input[1],input[2],input[3]], 
+      led2: [input[4],input[5],input[6]], 
+      time: input[7]
+    };
+  }
+  if(input.length === 1){
+    var action = {
+      act: input[0]
+    };
+  }
+  if(input.length === 4){
+    var action = {
+      act: input[0],
+      content: input[1],
+      size: input[2],
+      clear: input[3]
+    };
+  }
+  return JSON.stringify(action);
+}
+
 stdin.addListener("data", function(d) {
   var input = d.toString().trim();
   console.log("you entered: '" + input + "'");
-  ws.send(input);
+  let json = buildJson(input);
+  console.log(json);
+  ws.send(json);
 });
--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 An intelligent Medicine Assistant
 
+## Logo
 ![logo](MB_Logo.png)
+
+## Architektur
+![logo](Architektur.jpg)
 
 Contributors:
 Valentin Barth, Laura Wernicke, Christopher Battista
