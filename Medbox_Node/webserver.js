c44a1c2f44c80ff9d8b3b32d8ab5b9356e52e9ef
caBattista <christopherbattista95@gmail.com>
2018-06-10 19:48:13 +0200

    deleted http and fs

--- Medbox_Node/webserver.js
+++ Medbox_Node/webserver.js
@@ -1,17 +1,7 @@
-var fs = require('fs');
 var express = require('express');
 var app = express();
-/*
-app.get('/', function (req, res) {
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-});
-*/
 
 app.use('/', express.static("web"));
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 8080!');
 });
